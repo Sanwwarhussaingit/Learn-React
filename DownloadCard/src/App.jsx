bb735d035c516604a75d3f9b98a6092cd109b8e9
Sanwwar <sanwwarcode@gmail.com>
2024-05-12 22:11:18 +0530

    Download Card

---
+++ DownloadCard/src/App.jsx
@@ -0,0 +1,12 @@
+import Card from "./Components/Card/Card";
+function App() {
+  return (
+    <>
+    <div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
+      <Card/>
+    </div>
+    
+    </>
+  );
+}
+export default App;
